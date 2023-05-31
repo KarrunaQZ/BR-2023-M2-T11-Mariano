82ceaacff6ffd819f95ab2a53c69f06ee4e965c2
KarrunaQZ <KarrunaQZ@users.noreply.github.com>
2023-05-30 22:10:57 -0300

    Atualização Aula 3

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,24 +1,26 @@
-from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 import pygame
+from pygame.sprite import Sprite
+
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
 Y_POS = 310
+Y_POS_DUCK = 340
 JUMP_VEL = 8.5
-Y_DUCK = 335
 
 
-class Dinossaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = 80
-        self.dino_rect.y = 310
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS
         self.step_index = 0
         self.jump_vel = JUMP_VEL
         self.dino_jump = False
         self.dino_run = True
         self.dino_duck = False
-        
+
     def update(self, user_input):
         if self.dino_run:
             self.run()
@@ -31,14 +33,15 @@ class Dinossaur:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        elif not self.dino_jump:
-            self.dino_duck = False
+        elif user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_run = False
+            self.dino_jump = False 
+            self.dino_duck = True
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_run = True
             self.dino_jump = False
-        
-        if user_input[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_run = False
-            self.dino_duck = True
+            self.dino_duck = False
+
         if self.step_index >= 10:
             self.step_index = 0
 
@@ -56,17 +59,17 @@ class Dinossaur:
             self.jump_vel -= 0.8
 
         if self.jump_vel < -JUMP_VEL:
-            self.dino_rect_y = Y_POS
-            self.jump_vel = JUMP_VEL
+            self.dino_rect.y = Y_POS
             self.dino_jump = False
-            self.dino_run = True
-                        
+            self.jump_vel = JUMP_VEL
+
     def duck(self):
-        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1] 
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
-        self.dino_rect.y = Y_DUCK
+        self.dino_rect.y = Y_POS_DUCK
         self.step_index += 1
-        
+        self.dino_duck = False
+
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
