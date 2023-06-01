90f903d067ff74a6da6db11fc8a4bfc524ab9aa6
KarrunaQZ <KarrunaQZ@users.noreply.github.com>
2023-06-01 16:49:30 -0300

    Aula 4 - Atualização de atividade

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,10 +1,9 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.text_utils import draw_message_component
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
-
-FONT_STYLE = "freesansbold.ttf"
 
 
 class Game:
@@ -20,6 +19,7 @@ class Game:
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.score = 0
+        self.high_score = 0
         self.death_count = 0
 
         self.player = Dinosaur()
@@ -37,11 +37,14 @@ class Game:
     def run(self):
         self.playing = True
         self.obstacle_manager.reset_obstacles()
+        self.game_speed = 20
+        self.score = 0       
+
         while self.playing:
             self.events()
             self.update()
             self.draw()
-
+            
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -57,6 +60,11 @@ class Game:
         self.score += 1
         if self.score % 100 == 0:
             self.game_speed += 5
+        self.update_high_score()
+            
+    def update_high_score(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
 
     def draw(self):
         self.clock.tick(FPS)
@@ -78,12 +86,15 @@ class Game:
         self.x_pos_bg -= self.game_speed
 
     def draw_score(self):
-        font = pygame.font.Font(FONT_STYLE, 22)
-        text = font.render(f"Score: {self.score}", True, (0, 0, 0))
-        text_rect = text.get_rect()
-        text_rect.center = (1000, 50)
-        self.screen.blit(text, text_rect)
-
+        draw_message_component(
+            f"Pontos: {self.score}",
+            self.screen,
+            pos_x_center=1000,
+            pos_y_center=50,
+            font_size = 22,
+            font_color=(0,0,0)
+        )
+        
     def handle_events_on_menu(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -98,13 +109,37 @@ class Game:
         half_screen_width = SCREEN_WIDTH // 2
 
         if self.death_count == 0:
-            font = pygame.font.Font(FONT_STYLE, 22)
-            text = font.render("Press any key to start", True, (0, 0, 0))
-            text_rect = text.get_rect()
-            text_rect.center = (half_screen_width, half_screen_height)
-            self.screen.blit(text, text_rect)
+            draw_message_component(
+                f'Press any key to start',
+                self.screen,
+                pos_y_center=half_screen_height - 40
+            )
         else:
-            self.screen.blit(ICON, (half_screen_width - 20, half_screen_height - 140))
+            draw_message_component("Pressione qualquer tecla para reiniciar o jogo.",
+                self.screen,
+                pos_y_center = half_screen_height + 140
+                )
+            
+            draw_message_component(
+                f"Sua pontuação: {self.score}",
+                self.screen,
+                pos_x_center=half_screen_height - 150
+            )
+
+            draw_message_component(
+                f"Tentativas: {self.death_count}",
+                self.screen, 
+                pos_x_center=900,
+                pos_y_center=300
+                )
+            
+            draw_message_component(
+                f"Recorde de pontuação: {self.high_score}",
+                self.screen,
+               pos_y_center=half_screen_height - 100                
+                )
+            
+            self.screen.blit(ICON, (half_screen_width - 40, half_screen_height - 30))
 
         pygame.display.update()
         self.handle_events_on_menu()
